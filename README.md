d800c27b85211cffc6c38a76940f25e25e6d4dcb
Snigi13 <65667637+Snigi13@users.noreply.github.com>
2020-05-30 18:01:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blue-print
+# staticSquare
+A static square made using Matter.js physics engine
---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    
+    box1=new box (200,200,20,20);
+    box2=new box (210,170,20,40);
+    ground1=new ground(200,390,400,20);
+    
+
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine); 
+
+    box1.display();
+    box2.display();
+    ground1.display();
+    
+}  
